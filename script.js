23fb8b77f3d0c9d897d67e2f949e02a18af81cfa
Ramazan Kubalov <k700pp@ya.ru>
2023-09-03 22:30:39 +0300

    Выполнил задание №11

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+const btn = document.querySelector('#btn');
+const input = document.querySelector('#text');
+const square = document.querySelector('#square');
+const circle = square.querySelector('#circle');
+const squareBtn = square.querySelector('#e_btn');
+const range = document.querySelector('#range');
 
 
+btn.addEventListener('click', () => {
+    square.style.backgroundColor = input.value;
+    input.value = ''
+});
 
+squareBtn.style.display = 'none';
 
+range.addEventListener('input', (e) => {
+    circle.style.width = `${e.target.value}%`;
+    circle.style.height = `${e.target.value}%`;
+})
 
